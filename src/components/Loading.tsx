274c01d86996340ab003bf2a4d6784becc99c853
Mikus Riekstins <mikus.riekstins@gmail.com>
2026-02-09 00:21:32 +0200

    Simplify Loading component

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -7,9 +7,14 @@ export interface LoadingProps extends React.ComponentPropsWithoutRef<'div'> {
 
 const Loading = React.forwardRef<HTMLDivElement, LoadingProps>(
   ({ size = 'medium', className = '', ...props }, ref) => {
-    const loadingClasses = `loading loading--${size} ${className}`.trim();
+    const loadingClasses = `loading ${className}`.trim();
+    const spinnerSize = `loading--${size}`;
 
-    return <div ref={ref} className={loadingClasses} {...props} />;
+    return (
+      <div ref={ref} className={`${loadingClasses} ${spinnerSize}`} {...props}>
+        <div className="spinner" />
+      </div>
+    );
   }
 );
 
